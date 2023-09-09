0378de71e116a6937fd05c07d0dbd9b41c2d3bb8
Amor Koloskova <amor.koloskova@index.ru>
2023-09-09 19:55:19 +0300

    Добавить новый экран

--- Right On Target/SecondViewController.swift
+++ Right On Target/SecondViewController.swift
@@ -4,6 +4,11 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func hideCurrentScene() {
+            self.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
 
--- Right On Target/VeryFirstViewController.swift
+++ Right On Target/VeryFirstViewController.swift
@@ -13,22 +13,22 @@ class VeryFirstViewController: UIViewController {
         super.viewDidLoad()
     }
     
-    lazy var viewController: ViewController  = getViewController()
-//    lazy var secondViewController: SecondViewController = getViewController()
+    lazy var secondViewController: SecondViewController  = getViewController()
+    lazy var viewController: ViewController = getViewController()
     
     private func getViewController <T>() -> T {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let viewController = storyboard.instantiateViewController(identifier: "\(T.self)")
         return viewController as! T
     }
-    
-//    @IBAction func showNextScreen() {
-//        self.present(viewController, animated: true, completion: nil)
-//    }
-    
+
     @IBAction func openColorGame() {
-            self.present(viewController, animated: true, completion: nil)
+            self.present(secondViewController, animated: true, completion: nil)
     }
     
+    
+    @IBAction func openNumberGame() {
+        self.present(viewController, animated: true, completion: nil)
+    }
     
 }
